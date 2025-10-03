d640642cd5a96425f6012ffafdba333bece2b4df
Ahmed Matem <ahmedmatem@gmail.com>
2025-10-03 11:18:08 +0300

    Add project files.

---
+++ FlightMS.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlightMS.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
